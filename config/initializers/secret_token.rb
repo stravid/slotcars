6d595e07174781041bde99afb092e12f2e3fe4b2
Dominik Guzei <dominik_guzei@gmx.at>
2012-02-11 20:00:44 +0100

    initial commit with rails 3.2 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Slotcars::Application.config.secret_token = '4c2054ab8566eeb580f0ddd260821899bcd922f4c01ce324f1a08268685f67255810bfcc3ac66ceec5b5ccae2ebda1755a8cade9dfdc343c2fb701fb333a6ffd'
