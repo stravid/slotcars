8b9788af894f75213ec3fa8bc8450d73a143a6e6
Dominik Guzei <dominik_guzei@gmx.at>
2012-03-21 15:17:48 +0100

    removes not yet needed devise functionality from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  # :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :username, :email, :password, :password_confirmation, :remember_me
+  # other are :password_confirmation, :remember_me
+  attr_accessible :username, :email, :password
 
   protected
 
