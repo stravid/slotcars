35f2b9cace219b95019761214dcaccaa344618d1
David Strauß <mail@stravid.com>
2012-03-22 11:44:36 +0100

    uses seeds.rb for seed data

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120321125106) do
+ActiveRecord::Schema.define(:version => 20120321124926) do
 
   create_table "tracks", :force => true do |t|
     t.datetime "created_at", :null => false
