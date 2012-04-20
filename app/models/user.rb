50fbb2a25341e7b2ea4dd5e86f85f06550917068
Dominik Guzei <dominik_guzei@gmx.at>
2012-04-20 16:27:06 +0200

    makes it possible to login with username OR email

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,12 @@ class User < ActiveRecord::Base
   # other are :password_confirmation, :remember_me
   attr_accessible :username, :email, :password
 
+  attr_accessor :login
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    login = conditions.delete(:login)
+    where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.strip.downcase }]).first
+  end
+
 end
