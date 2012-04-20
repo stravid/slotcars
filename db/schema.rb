f54539804fdae153e5daf54dec692c9dab07212c
David Strauß <mail@stravid.com>
2012-04-20 21:55:50 +0200

    sets higher limit for track attributes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120420092842) do
+ActiveRecord::Schema.define(:version => 20120420195423) do
 
   create_table "runs", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -22,10 +22,10 @@ ActiveRecord::Schema.define(:version => 20120420092842) do
   end
 
   create_table "tracks", :force => true do |t|
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-    t.string   "raphael"
-    t.string   "rasterized"
+    t.datetime "created_at",                  :null => false
+    t.datetime "updated_at",                  :null => false
+    t.text     "raphael",    :limit => 64000
+    t.text     "rasterized", :limit => 64000
   end
 
   create_table "users", :force => true do |t|
