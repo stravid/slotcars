9806da71d32ac1158f47ce872724f326d3cbfb51
Thomas Buchöster <thomas.buchoester@gmail.com>
2012-04-20 12:33:48 +0200

    fixes track validation (server-side) to prevent internal server errors

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,7 +1,7 @@
 class Track < ActiveRecord::Base
 
   # regualar expression assures at least 3 points
-  validates :raphael, format: { with: /\AM(R?(\d{1,4}(\.\d{2})?),?){6,}z\z/ }
+  validates :raphael, format: { with: /\AM(R?(\d{1,4}(\.\d{1,2})?),?){6,}z\z/ }
   validates :rasterized, :presence => true
 
 end
