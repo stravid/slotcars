444889ebe80ed033f9dcd9479106c9b0b1422459
Dominik Guzei <dominik_guzei@gmx.at>
2012-04-20 19:58:12 +0200

    fixes problem with sessions specs to use login instead of email

--- spec/controllers/api/sessions_controller_spec.rb
+++ spec/controllers/api/sessions_controller_spec.rb
@@ -15,14 +15,14 @@ describe Api::SessionsController do
       serializer = UserSerializer.new user, :root => "user"
       serialized_user = serializer.as_json
 
-      post :create, { :user => { :email => user.email, :password => user.password } }
+      post :create, { :user => { :login => user.email, :password => user.password } }
 
       response.body.should == serialized_user.to_json
       response.should be_success
     end
 
     it 'should return 401 unauthorized when login failed' do
-      post :create, { :user => { :email => "wrong", :password => "data" } }
+      post :create, { :user => { :login => "wrong", :password => "data" } }
 
       response.code.should eq '401'
     end
